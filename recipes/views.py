0521d6c8362e3379c6465f3a833081d213ea3452
Gelson Agostinho <gelsonagostino@msn.com>
2024-06-28 23:28:43 +0100

    primeira fase do site terminada

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, "recipes/pages/home.html")
+    return render(
+        request,
+        "recipes/pages/home.html",
+        context={"recipes": [make_recipe() for _ in range(10)]},
+    )
+
+
+def recipes(request, _id):
+    return render(
+        request,
+        "recipes/pages/recipe.html",
+        context={
+            "recipe": make_recipe(),
+            "is_detail_page": True,
+        },
+    )
---
+++ utils/recipes/factory.py
@@ -0,0 +1,45 @@
+# from inspect impont signatuae
+
+from random import randint
+
+from faker import Faker
+
+
+def rand_ratio():
+    return randint(840, 900), randint(473, 573)
+
+
+fake = Faker("pt-BR")
+
+# print(signature(fake.random_number))
+
+
+def make_recipe():
+    return {
+        "id": fake.random_number(digits=3, fix_len=True),
+        "title": fake.sentence(nb_words=4),
+        "description": fake.sentence(nb_words=12),
+        "preparation_time": fake.random_number(digits=2, fix_len=True),
+        "preparation_time_unit": "Minutos",
+        "servings": fake.random_number(digits=2, fix_len=True),
+        "servings_unit": "Porções",
+        "preparation_steps": fake.text(3000),
+        "created_at": fake.date_time(),
+        "updated_at": fake.date_time(),
+        "author": {
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+        },
+        "category": {
+            "name": fake.word(),
+        },
+        "cover": {
+            "url": f"https://loremflickr.com/{rand_ratio()[0]}/{rand_ratio()[1]}/food,cook",
+        },
+    }
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    pprint(make_recipe()["title"])
